9671f146a2d126107cd84c2ea45f55b50598d110
Ivancabano <demonivo0@gmail.com>
2022-04-14 14:19:59 -0300

    prototipo_de_menu_2

--- tp_1/src/tp_1.c
+++ tp_1/src/tp_1.c
@@ -14,30 +14,75 @@
 int pedidoDeDatoDeUnEntero();
 int main(void) {
 	setbuf(stdout,NULL);
-
+   int respuestaDeSalida;
    int opcionDelMenu;
 
-
+do{
    printf("1. Ingresar Kilometros : \n");
    printf("2. ingresar Precio de Vuelos : \n");
    printf("3. Calcular todos los costos : \n");
    printf("4. Informar resultados  \n");
    printf("5. Carga forzada de datos  \n");
    printf("6. Salir \n");
-
+  scanf("%d",&opcionDelMenu);
 
    switch(opcionDelMenu){
    case 1 :
+	  do{
+		  printf("1. Ingresar Kilometros : \n");
+		  printf("2 salir : \n");
+          scanf("%d",&opcionDelMenu);
 
-
+	   }while(opcionDelMenu != 2);
 
    break;
 
+   case 2 :
+
+	   do{
+	 		  printf("1. Ingresar Kilometros : \n");
+	 		  printf("2 salir : \n");
+	           scanf("%d",&opcionDelMenu);
+
+	 	   }while(opcionDelMenu != 2);
+
+      break;
+
+   case 3 :
+
+	   do{
+	 		  printf("1. Ingresar Kilometros : \n");
+	 		  printf("2 salir : \n");
+	           scanf("%d",&opcionDelMenu);
+
+	 	   }while(opcionDelMenu != 2);
+
+      break;
+   case 4 :
+
+	   do{
+	 		  printf("1. Ingresar Kilometros : \n");
+	 		  printf("2 salir : \n");
+	           scanf("%d",&opcionDelMenu);
+
+	 	   }while(opcionDelMenu != 2);
+
+      break;
+   case 5 :
+
+	   do{
+	 		  printf("1. Ingresar Kilometros : \n");
+	 		  printf("2 salir : \n");
+	           scanf("%d",&opcionDelMenu);
+
+	 	   }while(opcionDelMenu != 2);
+
+      break;
 
    }
 
 
-
+}while(opcionDelMenu != 6);
 
 
 	return  0;
